3fe45a49c2676992ca68851d8eb3de56ffe4eb63
mitkousa <uk.dimitar@gmail.com>
2021-08-10 20:48:32 +0100

    console ready

--- console.py
+++ console.py
@@ -8,6 +8,18 @@ import repositories.album_repository as album_repository
 album_repository.delete_all()
 artist_repository.delete_all()
 
+album1 = Album('Pulse', 'Pop', artist1)
+album_repository.save(album1)
+
+album2 = Album('Thriller', 'Rhythm and blues', artist2)
+album_repository.save(album2)
+
+artist1 = Artist('Toni Braxton')
+album_repository.save(artist1)
+
+artist2 = Artist('Michael Jackson')
+album_repository.save(artist1)
+
 
 
 for album in album_repository.select_all():
